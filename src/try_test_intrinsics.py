6cfbf34006b261bb8fe18dc1318fac015f38c035
stevenjiang <stevenjiang1110@gmail.com>
2020-09-16 12:20:48 +0800

    fix

--- src/try_test_intrinsics.py
+++ src/try_test_intrinsics.py
@@ -10,18 +10,18 @@ def run(filename):
     setAngrLogger(logging.INFO, True)
     disassembly_filename = filename + ".txt"
     #lang_start
-    lang_start_symbol = rust_util.get_one_symbol(disassembly_filename,rust_util.std_rt_lang_start)
-    proj.hook_symbol(lang_start_symbol, lang_start())
+    #lang_start_symbol = rust_util.get_one_symbol(disassembly_filename,rust_util.std_rt_lang_start)
+    #proj.hook_symbol(lang_start_symbol, lang_start())
     #print
-    print_symbol = rust_util.get_one_symbol(disassembly_filename, rust_util.std_io_stdio__print)
-    proj.hook_symbol(print_symbol, angr.SIM_PROCEDURES['libc']['printf']())
+    #print_symbol = rust_util.get_one_symbol(disassembly_filename, rust_util.std_io_stdio__print)
+    #proj.hook_symbol(print_symbol, angr.SIM_PROCEDURES['libc']['printf']())
     #panic
     #可能存在多个panic的签名
-    panic_symbols = rust_util.get_all_symbols(disassembly_filename, rust_util.std_panicking_begin_panic)
-    for panic_symbol in panic_symbols:
-        proj.hook_symbol(panic_symbol, rust_panic())
+    #panic_symbols = rust_util.get_all_symbols(disassembly_filename, rust_util.std_panicking_begin_panic)
+    #for panic_symbol in panic_symbols:
+    #    proj.hook_symbol(panic_symbol, rust_panic())
     
-    state = proj.factory.entry_state()
+    state = proj.factory.entry_state(add_options=angr.options.unicorn)
     simgr = proj.factory.simulation_manager(state)
     simgr.run()
     print(simgr.deadended)
